2846b6351ce585a542a80bf1963536ba804beeac
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-11-11 23:50:38 +0100

    Added links and a basic login-form

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,11 @@
 module.exports = function setup(app) {
-	app.get('/', function(req, res) {
-		res.render('index')
-	})
+	app.get('/', render('index'))
+	app.get('/signup', render('users/form', { action: 'users', 'btn-text': 'Signup' }))
+	app.get('/login', render('users/form', { action: 'login', 'btn-text': 'Login' }))
+}
+
+function render(view, data) {
+	return function(req, res) {
+		res.render(view, data)
+	}
 }
