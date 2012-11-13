6516bf0167a4a527146aed61a50b47901a1b79a5
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-11-13 09:12:25 +0100

    Fixed a broken test (test-data had become invalid)

--- test/web.api/events.js
+++ test/web.api/events.js
@@ -11,8 +11,8 @@ describe('web.api/events.js', function() {
 		it('returns a list of events', function() {
 			var expected =
 			    [ { id: 1
-			      , start: '2012-12-05T19:00:00'
-			      , end: '2012-12-07T14:00:00'
+			      , start: '2012-12-07T19:00:00'
+			      , end: '2012-12-09T14:00:00'
 			      }
 			    ]
 			return expect(client.get('/events').get(1))
