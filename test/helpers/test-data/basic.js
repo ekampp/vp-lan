67982b0c64e4e3c7c9777388993623014f29ad02
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-11-26 22:42:48 +0100

    Updated event-data to have properly formatted dates.

--- test/helpers/test-data/basic.js
+++ test/helpers/test-data/basic.js
@@ -18,8 +18,8 @@ function setData() {
 	, storage.events.add(
 	    { id: 1
 	    , name: 'December 2012'
-	    , start: '2012-12-07T19:00:00'
-	    , end: '2012-12-09T14:00:00'
+	    , start: '2012-12-07T19:00:00+01:00'
+	    , end: '2012-12-09T14:00:00+01:00'
 	    , tables:
   '.| .|\n'
 + '.| .|\n'
--- test/web.api/events.js
+++ test/web.api/events.js
@@ -78,8 +78,8 @@ describe('web.api/events.js', function() {
 		it('returns a list of events', function() {
 			var expected =
 			    [ { id: 1
-			      , start: '2012-12-07T19:00:00'
-			      , end: '2012-12-09T14:00:00'
+			      , start: '2012-12-07T19:00:00+01:00'
+			      , end: '2012-12-09T14:00:00+01:00'
 			      , seats:
 			        [ /* Any seats will do. We will test
 			           * the actual seats in another file
@@ -95,8 +95,8 @@ describe('web.api/events.js', function() {
 		it('returns a single event', function() {
 			var expected =
 			    { id: 1
-			    , start: '2012-12-07T19:00:00'
-			    , end: '2012-12-09T14:00:00'
+			    , start: '2012-12-07T19:00:00+01:00'
+			    , end: '2012-12-09T14:00:00+01:00'
 			    , seats:
 			      [ /* Any seats will do. We will test
 			         * the actual seats in another file
