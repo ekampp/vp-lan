eba360db25e5548041c719892417ade17247216d
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-12-05 23:52:32 +0100

    Fixed an issue where setting the header directly in a call would override the default header options that was set when the http-helper was created.

--- test/helpers/http-helper.js
+++ test/helpers/http-helper.js
@@ -65,7 +65,17 @@ function createHelper(defaults, options) {
 			out[key] = a[key]
 		})
 		Object.keys(b || {}).forEach(function(key) {
-			out[key] = b[key]
+			var val = b[key]
+			// We only want to do this for actual objects
+			// Any falsy type is not an actual object (0, '', null, etc)
+			if(val
+			&& typeof(val) == 'object'
+			&& a[key]
+			&& typeof(a[key]) == 'object'
+			) {
+				val = merge(a[key], val)
+			}
+			out[key] = val
 		})
 		return out
 	}
